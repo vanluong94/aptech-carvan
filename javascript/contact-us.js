92575abb4f17633443679bb04aacb80e120536f5
kieuanhdavid <kieuanhdavid@gmail.com>
2022-03-11 04:36:51 +0700

    test another commit

--- javascript/contact-us.js
+++ javascript/contact-us.js
@@ -22,29 +22,29 @@ function validateForm(){
         let notiButton = document.getElementById('notiButton');
 
         sr_email.textContent = 'The e-mail address entered is invalid.';
-        sr_email.style.fontSize = '16px';
+        sr_email.style.fontSize = '13px';
         sr_email.style.color = 'red';
         sr_email.style.margin = 0;
 
         notiButton.textContent = 'One or more fields have an error. Please check and try again.'
-        notiButton.style.fontSize = '16px';
+        notiButton.style.fontSize = '13px';
         notiButton.style.color = 'black';
         notiButton.style.border = "2px solid #ffb900";
         notiButton.style.textAlign = 'center';
-
+        
         document.formContact.txtEmail.focus();
         return false
     }
     if (emailPattern.test(document.formContact.txtEmail.value) == true) {
 
         notiButton.textContent = 'Thank you for your message. It has been sent.'
-        notiButton.style.fontSize = '16px';
+        notiButton.style.fontSize = '13px';
         notiButton.style.color = 'black';
         notiButton.style.border = "2px solid #46b450";
         notiButton.style.textAlign = 'center';
 
         document.formContact.txtEmail.focus();
-        return true
+        return false
     }
 }
 
